6a763caef8df25f201cd18015835fdcfd264ca75
Axel <44190680+MLRAleader@users.noreply.github.com>
2024-04-01 10:21:48 +0100

    Delete assets directory

--- assets/js/audioPlay.js
+++
@@ -1,7 +0,0 @@
-var imageProfile = document.querySelector("#image-profile");
-  imageProfile.addEventListener('click', playPresentation);
-  
-  function playPresentation() {
-    var presentation = new Audio("/assets/medias/audios/Presentation_Succinte.aac");
-    presentation.play();
-  }
